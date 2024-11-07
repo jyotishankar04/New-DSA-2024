ac4773d2604c07237063c3a7ea079e346b0c712a
jyotishankar04 <patrajyotishankar@gmail.com>
2024-11-07 13:19:24 +0530

    string problem 1

--- string/basicString.cpp
+++ string/basicString.cpp
@@ -1,11 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class basicString
 {
 public:
+    void reverseString(vector<char> &);
+    bool isPalindrome(string &);
 };
 
+void basicString::reverseString(vector<char> &s)
+{
+    int n = s.size();
+    int left = 0;
+    int right = n - 1;
+    while (left < right)
+    {
+        swap(s[left], s[right]);
+        left++;
+        right--;
+    }
+}
+
+bool basicString::isPalindrome(string &s)
+{
+    int size = s.length();
+    int left = 0;
+    int right = size - 1;
+
+    while (left < right)
+    {
+        if (s[left] != s[right])
+        {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
 int main()
 {
+
     basicString obj;
+    vector<char> v = {'h', 'e', 'l', 'l', 'o'};
+    obj.reverseString(v);
+    cout << "Reversed string: ";
+    for (auto it : v)
+    {
+        cout << it << " ";
+    }
+    cout << endl;
+
+    string s1 = "zyz";
+    string result = obj.isPalindrome(s1) ? "True" : "False";
+    cout << "Check Palindrome: " << result << endl;
 
     return 0;
 }
