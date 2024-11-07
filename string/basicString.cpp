637c8e12e5aac56f224e1da3f44e28e5bb20b8ec
jyotishankar04 <patrajyotishankar@gmail.com>
2024-11-07 14:54:48 +0530

    Largest odd Substring Number

--- string/basicString.cpp
+++ string/basicString.cpp
@@ -6,6 +6,7 @@ class basicString
 public:
     void reverseString(vector<char> &);
     bool isPalindrome(string &);
+    string largestSubStringNumber(string &);
 };
 
 void basicString::reverseString(vector<char> &s)
@@ -38,22 +39,52 @@ bool basicString::isPalindrome(string &s)
     }
     return true;
 }
+
+string basicString::largestSubStringNumber(string &s)
+{
+    int n = s.length();
+    int j = -1;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        if ((s[i] - '0') % 2 == 1)
+        {
+            j = i;
+            break;
+        }
+    }
+    if (j == -1)
+        return "";
+
+    int i = 0;
+    while (i < n)
+    {
+        if (s[i] != '0')
+            break;
+        i++;
+    }
+    return s.substr(i, j - i + 1);
+}
+
 int main()
 {
 
     basicString obj;
-    vector<char> v = {'h', 'e', 'l', 'l', 'o'};
-    obj.reverseString(v);
-    cout << "Reversed string: ";
-    for (auto it : v)
-    {
-        cout << it << " ";
-    }
-    cout << endl;
+    // vector<char> v = {'h', 'e', 'l', 'l', 'o'};
+    // obj.reverseString(v);
+    // cout << "Reversed string: ";
+    // for (auto it : v)
+    // {
+    //     cout << it << " ";
+    // }
+    // cout << endl;
 
-    string s1 = "zyz";
-    string result = obj.isPalindrome(s1) ? "True" : "False";
-    cout << "Check Palindrome: " << result << endl;
+    // string s1 = "zyz";
+    // string result = obj.isPalindrome(s1) ? "True" : "False";
+    // cout << "Check Palindrome: " << result << endl;
+
+    // string s2 = "00093838";
+    // string result = obj.largestSubStringNumber(s2);
+    // cout << "Largest Substring Number: " << result << endl;
 
     return 0;
 }
