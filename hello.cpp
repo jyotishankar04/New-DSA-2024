5c8c7e6553c45bc11d0d97b58a1059ef14acbe8a
jyotishankar04 <patrajyotishankar@gmail.com>
2025-10-14 21:35:59 +0530

    adding sorting folder and implemetented selection and bubble sort

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string name;
+    cout << "Enter your name: " << endl;
+    cin >> name;
+    cout << "Hello, " << name << "!" << endl;
+    
+    return 0;
+}
---
+++ sorting/sorting-main.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Selection Sort   
+
+// [4,7,2,5,3,1]
+void selectionSort(vector<int> & arr, int n){
+
+    for(int i = 0; i < n - 1; i++){
+        int minIndex = i;
+        for(int j = i + 1; j < n; j++){
+            if(arr[j] < arr[minIndex]){
+                minIndex = j;
+            }
+        }
+        if(i != minIndex){
+            swap(arr[i],arr[minIndex]);
+        }
+    }
+}
+
+// Bubble sort
+// [4,7,2,5,3,1]
+
+void bubbleSort(vector<int> & arr, int n){
+    for(int i = n -1 ; i >= 0; i--){
+        for(int j = 0; j <= i; j ++){
+            if(arr[j] > arr[j + 1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
+
+int main(){
+vector<int> arr;
+    int n;
+    cout << "Enter the SIZE of the array: ";
+    cin >> n;
+
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        arr.push_back(x);
+    }
+
+    cout << "Unsorted Array: ";
+    for (auto it : arr) cout << it << " ";
+    cout << endl;
+
+    // selectionSort(arr, n);
+    bubbleSort(arr, n);
+    cout << "After sorting the array: ";
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+    cout << endl;
+
+    return 0;
+}
