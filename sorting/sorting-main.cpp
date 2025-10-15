826cb31a2ea3b3df5a064be056c448a1ef98d9a4
jyotishankar04 <patrajyotishankar@gmail.com>
2025-10-15 20:40:31 +0530

    learned  mergesort

--- sorting/sorting-main.cpp
+++ sorting/sorting-main.cpp
@@ -41,7 +41,47 @@ void insertionSort(vector<int> & arr, int n){
         }
     }
 }
+void merge(vector<int> & arr, int low, int mid, int high){
+    vector<int> tempArr;
+    int left = low;
+    int right = mid + 1;
 
+    while (left <= mid && right <= high)
+    {   
+        if(arr[left] >= arr[right]){
+            tempArr.push_back(arr[right]);
+            right++;
+        }
+        if(arr[left] < arr[right]){
+            tempArr.push_back(arr[left]);
+            left++;
+        }
+    }
+    while (left <= mid)
+    {
+        tempArr.push_back(arr[left]);
+        left++;
+    }
+    while (right <= high)
+    {
+        tempArr.push_back(arr[right]);
+        right++;
+    }
+    for (int i = low; i <= high; i++)
+    {   
+        arr[i] = tempArr[i - low];
+    }
+    
+}
+
+void mergeSort(vector<int> & arr, int low,int high){
+    if(low >= high) return;
+    int mid = (low + high)/2;
+
+    mergeSort(arr,low, mid);
+    mergeSort(arr,mid+1, high);
+    merge(arr, low, mid, high);
+}
 
 
 int main(){
@@ -63,7 +103,9 @@ vector<int> arr;
 
     // selectionSort(arr, n);
     // bubbleSort(arr, n);
-    insertionSort(arr,n);
+    // insertionSort(arr,n);
+    mergeSort(arr,0,n-1);
+
     cout << "After sorting the array: ";
     for (int i = 0; i < n; i++) cout << arr[i] << " ";
     cout << endl;
