08e816edcf19f482e0dc2e61f3e678beaf0a2624
jyotishankar04 <patrajyotishankar@gmail.com>
2025-10-20 12:28:46 +0530

    rotate array optimal approach

--- array/rotateArray.cpp
+++ array/rotateArray.cpp
@@ -3,11 +3,23 @@ using namespace std;
 
 class rotateArray
 {
+    private:
+    void reverseArray(vector<int> & arr, int start, int end){
+        while (start <  end)
+        {
+            int temp = arr[start];
+            arr[start]  = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
     public:
     void rotateArrayByKBrute(vector<int> & arr, int k){
         if(arr.size() == k){
             return;
         }
+        k = k % arr.size();
         for (int i = 0; i < k; i++)
         {   
             int temp = arr[0];
@@ -17,6 +29,30 @@ class rotateArray
             }
             arr[arr.size()-1] = temp;
         }
+    }
+
+    void rotateArrayByKBatter(vector<int> & arr, int k){
+        int n = arr.size();
+        k = k % n;
+        vector<int> temp;
+        for (int i = 0; i < k; i++)
+        {
+            temp.push_back(arr[i]);  
+        }
+        for(int i = 0; i <n - k; i ++){
+            arr[i] = arr[i + 1];
+        }
+        // for(int i = (n - k); i < n; i++){
+        //     arr[i] = temp[n - k - i];
+        // }
+    }
+    void rotateArrayByKOptimal(vector<int> & arr, int k){
+        int n = arr.size();
+        k = k%n; // ! This like ensures that if the k is grater the length of the array then we should take the remainder - this ersures the unnecessary loops
+        
+        this->reverseArray(arr, 0,k-1);
+        this->reverseArray(arr,k, n-1);
+        this->reverseArray(arr,0, n-1);
     }
 };
 
@@ -42,7 +78,9 @@ int main()
     }
     cout << endl;
      rotateArray rotateArr; 
-    rotateArr.rotateArrayByKBrute(arr,3);
+    // rotateArr.rotateArrayByKBrute(arr,8);
+    // rotateArr.rotateArrayByKBatter(arr,3);
+    rotateArr.rotateArrayByKOptimal(arr,3);
     cout<<"After Rotate: ";
     for (auto it : arr)
     {
---
+++ array/tempCodeRunnerFileer.cpp
@@ -0,0 +1,3 @@
+for(int i = (n - k); i < n; i++){
+        //     arr[i] = temp[n - k - i];
+        // }
