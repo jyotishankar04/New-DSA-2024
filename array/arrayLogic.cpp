52566aa0f6e397cf4fa5ea7561ee6a0b4739c779
jyotishankar04 <patrajyotishankar@gmail.com>
2025-11-03 17:27:43 +0530

    added intersection

--- array/arrayLogic.cpp
+++ array/arrayLogic.cpp
@@ -10,6 +10,8 @@ public:
     int missingElementBetter(vector<int> &arr);
     int missingElementOptimal(vector<int> &arr);
     int missingElementOptimal2(vector<int> &arr);
+    vector<int> intersectionOfTwoArraysBrute(vector<int> &arr1, vector<int> &arr2);
+    vector<int> intersectionOfTwoArraysOptimal(vector<int> &arr1, vector<int> &arr2);
 };
 
 void arrayLogic::moveZeroToEnd(vector<int> &arr)
@@ -72,7 +74,7 @@ int arrayLogic::missingElementBrute(vector<int> &arr)
         int flag = 0;
 
         // Search for the element using linear search
-        for (int j = 0; j < N; j++) 
+        for (int j = 0; j < N; j++)
         {
             if (arr[j] == i)
             {
@@ -135,6 +137,60 @@ int arrayLogic::missingElementOptimal2(vector<int> &arr)
     }
     return sum;
 }
+
+vector<int> arrayLogic::intersectionOfTwoArraysBrute(vector<int> &arr1, vector<int> &arr2)
+{
+    int n1 = arr1.size();
+    int n2 = arr2.size();
+    vector<int> result;
+    vector<int> vistied(n2, 0);
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < n2; j++)
+        {
+            if (arr1[i] == arr2[j] && vistied[j] == 0)
+            {
+                result.push_back(arr1[i]);
+                vistied[j] = 1;
+                break;
+            }
+            else if (arr1[i] > arr2[j])
+            {
+                break;
+            }
+        }
+        return result;
+    }
+}
+
+vector<int> arrayLogic::intersectionOfTwoArraysOptimal(vector<int> &arr1, vector<int> &arr2)
+{
+    int n1 = arr1.size();
+    int n2 = arr2.size();
+    vector<int> result;
+    int i = 0;
+    int j = 0;
+    while (i < n1 && j < n2)
+    {
+        if (arr1[i] == arr2[j])
+        {
+            result.push_back(arr1[i]);
+            i++;
+            j++;
+        }
+        else if (arr1[i] < arr2[j])
+        {
+            i++;
+        }
+        else
+        {
+            j++;
+        }
+    }
+
+    return result;
+}
+
 int main()
 {
     vector<int> arr;
@@ -169,12 +225,12 @@ int main()
 
     // ! Bellow code is for the missing element
 
-    int missing = logic.missingElementBrute(arr);
+    // int missing = logic.missingElementBrute(arr);
     // int missing = logic.missingElementBetter(arr);
     // int missing = logic.missingElementOptimal(arr);
     // int missing = logic.missingElementOptimal2(arr);
 
-    cout << "The missing element is : " << missing << endl;
+    // cout << "The missing element is : " << missing << endl;
 
     // int withoutDuplicates = logic.removeDuplicatesOptimal(arr);
 
@@ -183,6 +239,26 @@ int main()
     // {
     //     cout << arr[i] << " ";
     // }
+
+    // ! For the intersection of two arrays
+    vector<int> arr1 = {1, 2, 3, 4, 5, 6, 7};
+    vector<int> arr2 = {1, 2, 3, 4, 5, 6, 7};
+
+    // ! For the intersection of two arrays
+    vector<int> result1 = logic.intersectionOfTwoArraysBrute(arr1, arr2);
+    vector<int> result2= logic.intersectionOfTwoArraysOptimal(arr1, arr2);
+
+    cout << "The intersection of two arrays using brute is : ";
+    for (auto it : result1)
+    {
+        cout << it << " ";
+    }
+    cout << endl;
+    cout << "The intersection of two arrays using optimal is : ";
+    for (auto it : result2)
+    {
+        cout << it << " ";
+    }
 
     return 0;
 }
