456d1b2ca5ffcf0cd7b8a8779c63dba409b0ca92
jyotishankar04 <patrajyotishankar@gmail.com>
2025-11-27 23:04:36 +0530

    majority practice

---
+++ array/medium/majority-element.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElementBrute(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        int ans = 0;
+        for(int i=0; i<n; i++){
+            if(count == 0){
+                ans = nums[i];
+                count = 1;
+            }
+            else if(nums[i] == ans){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return ans;
+    }
+    int majorityElementBatter(vector<int>& nums) {
+        unordered_map<int, int> hash;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            hash[nums[i]]++;
+        }
+        for (auto it : hash)
+        {
+            if(it.second > n/2) return it.first;
+        }
+        return -1;
+    }
+    int majorityElementOptimal(vector<int> & nums){
+        int n = nums.size();
+        int cnt = 0;
+        int el;
+        for (int i = 0; i < n; i++)
+        {
+            if(cnt == 0){
+                el = nums[i];
+                cnt = 1;
+            }else if(nums[i] == el){
+                cnt++;
+            }else{
+                cnt--;
+            }
+        }
+        int finalCount = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if(nums[i] == el){
+                finalCount++;
+            }
+        }
+        return finalCount > n/2 ? el : -1;
+    }
+};
+
+int main(){
+    vector<int> nums = {2,2,1,1,1,2,2};
+    Solution obj;
+    cout<<"Majority Element - Brute Approach: "<<obj.majorityElementBrute(nums)<<endl;
+    cout << "Majority Element - Batter Approach: " << obj.majorityElementBatter(nums) << endl;
+    cout << "Majority Element - Optimal Approach: " << obj.majorityElementOptimal(nums) << endl;
+
+    return 0;
+}
